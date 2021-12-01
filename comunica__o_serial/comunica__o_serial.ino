c5c77976ee228b28eda991732389ef80cd0dd7b5
GabrielTibolaCastagnera <86370643+GabrielTibolaCastagnera@users.noreply.github.com>
2021-12-01 10:55:18 -0300

    Add files via upload

--- comunica__o_serial/comunica__o_serial.ino
+++ comunica__o_serial/comunica__o_serial.ino
@@ -1,16 +1,23 @@
+//inclusão das bibliotecas necessárias
 #include <DHT.h>
 #include <DHT_U.h>
 #include <LiquidCrystal.h>
+
+//definição das porta
 #define DHT11_sensor A1
 #define DHTTYPE    DHT11
 #define humity_sensor A0
 int rs = 12, e = 11, DB4 = 5, DB5 = 4, DB6 = 3, DB7 = 2;
 LiquidCrystal lcd(rs, e, DB4, DB5, DB6, DB7);
 DHT_Unified dht(DHT11_sensor, DHTTYPE);
+
+//cabeçalhos das funções
 void WriteTemperatureAndHumityFromEnvairoment();
 void WriteHumityFromGround(int humity, int _Maximum_Ground_Humity, int _Minimum_Ground_Humity);
+
+//seta as entradas e saídas e inicializa o LCD e o sensor de temperatura e umidade
 void setup() {
-  Serial.begin(57600);
+  Serial.begin(115200);
   pinMode(13, OUTPUT);
   pinMode(humity_sensor, INPUT);
   digitalWrite(13, LOW);
@@ -19,37 +26,58 @@ void setup() {
   lcd.setCursor(0, 0);
 
 }
+
+//declaração das variáveis globais
 int Maximum_Ground_Humity = -1, Minimum_Ground_Humity = -1;
 char Maximum_Humity[10] = "111", Minimum_Humity[10] = "111";
-char estado = '0';
+int estado = '0';
 bool LED = true;
 unsigned long temporizer = millis();
-void loop() {
 
+//configuração de solo seco e molhado
+int dry_ground = 1023, wet_ground = 390;
+
+
+void loop() {
+  //Escreve no dysplay LCD a umidade e a Temperatura do meio
   WriteTemperatureAndHumityFromEnvairoment();
-  if (estado == '1')
+
+  //estado 1 se as informações de máxima e mínima umidade do solo estiverem corretas
+
+  if (Serial.available() > -1)
   {
-    if ((millis() - temporizer) > 1000)
+
+
+    
+
+    //lê do computador qual será a tarefa a ser feita
+    if (estado == '1')
     {
-      int humity;
-      digitalWrite(13, HIGH);
-      WriteTemperatureAndHumityFromEnvairoment();
-      humity = constrain(analogRead(humity_sensor), 390, 1023);
-      humity = map (humity, 390, 1023, 100, 0);
-      WriteHumityFromGround(humity, Maximum_Ground_Humity, Minimum_Ground_Humity);
-      digitalWrite(13, LED);
-      LED = !LED;
-      temporizer = millis();
+      //atualiza a umidade do solo a cada 1 segundos. Pisca um LED para saber que está funcionando
+      if ((millis() - temporizer) > 1000)
+      {
+        int humity;
+        digitalWrite(13, HIGH);
+
+        //captura qual é a umidade atual do solo em %
+        humity = constrain(analogRead(humity_sensor), wet_ground, dry_ground);
+        humity = map (humity, wet_ground, dry_ground, 100, 0);
+        //Envia a umidade atual do solo e se precisa de mais água ou não para o computador
+        WriteHumityFromGround(humity, Maximum_Ground_Humity, Minimum_Ground_Humity);
+
+        digitalWrite(13, LED);
+        LED = !LED;
+        temporizer = millis();
+
+      }
+
 
     }
 
-
-  }
-  if (Serial.available() > 0)
-  {
-    char aux = estado;
+    int aux = estado;
     estado = Serial.read();
-    
+
+    //estado 3 para quando o usuário quiser parar o circuito e depois voltar para o estado anterior
     if (estado == '3')
     {
       estado = aux;
@@ -60,17 +88,18 @@ void loop() {
         delay(250);
         digitalWrite(13, LOW);
         delay(250);
-        
+
         estado = Serial.read();
       }
       estado = aux;
     }
+    //estado 2 para atualizar os dados de máxima e de mínima. Caso não estiverem corretos, o programa exibirá uma mensagem e não exibirá a umidade do solo
     if ((estado == '2'))
     {
-      Maximum_Ground_Humity = Serial.read();
-      Minimum_Ground_Humity = Serial.read();
-      
-      
+      Maximum_Ground_Humity = Serial.parseInt();
+      Minimum_Ground_Humity = Serial.parseInt();
+
+
       if (Maximum_Ground_Humity >= 0 && Maximum_Ground_Humity <= 100 && Minimum_Ground_Humity <= Maximum_Ground_Humity && Minimum_Ground_Humity >= 0)
       {
         estado = '1';
@@ -90,15 +119,18 @@ void loop() {
 
 
   }
-
+  
 
 }
+//função que lê e mostra no LCD as informações de temperatura e umidade do meio
 void WriteTemperatureAndHumityFromEnvairoment()
 {
   sensors_event_t eventT, eventH;
   dht.temperature().getEvent(&eventT);
   dht.humidity().getEvent(&eventH);
   lcd.setCursor(0, 0);
+
+
   if (isnan(eventT.temperature)) {
     lcd.println(F("Error reading the envairoment temperature!"));
   }
@@ -107,6 +139,8 @@ void WriteTemperatureAndHumityFromEnvairoment()
     lcd.print("Temperature C:");
     lcd.println(int(eventT.temperature));
   }
+
+
   if (isnan(eventH.relative_humidity)) {
     lcd.println(F("Error reading the envairoment humidity!"));
   }
@@ -116,6 +150,8 @@ void WriteTemperatureAndHumityFromEnvairoment()
     lcd.print(int(eventH.relative_humidity));
   }
 }
+
+//função que envia os dados de umidade do solo e se precisa mais água para o computador
 void WriteHumityFromGround(int humity, int _Maximum_Ground_Humity, int _Minimum_Ground_Humity)
 {
   Serial.print("Ground Humity: ");
